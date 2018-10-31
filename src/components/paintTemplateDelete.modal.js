f65224547c2fcac1b780333b69ca9fb69fe328ec
Gbacc <g.baccarini@free.fr>
2018-10-31 16:48:32 +0100

    Close button for modal

--- src/components/paintTemplateDelete.modal.js
+++ src/components/paintTemplateDelete.modal.js
@@ -9,14 +9,15 @@ const PaintTemplateDelete = ({ isOpen, currentTemplate, handleDelete, handleClos
             <div className="modal-overlay"></div>
             <div className="modal-container">
                 <div className="modal-header">
+                    <a className="btn btn-clear float-right" href="#modals" aria-label="Close" onClick={handleClose}></a>
                     <div className="modal-title h5">{currentTemplate.label}</div>
                 </div>
                 <div className="modal-body">
                     <p>Do you want to delete this template </p>
                 </div>
                 <div className="modal-footer">
-                    <button id="submitModal" className="btn btn-primary" onClick={()=>handleDelete(currentTemplate)}>Delete</button>
-                    <button id="closeModal" className="btn btn-link" onClick={()=>handleClose()}>Close</button>
+                    <button id="submitModal" className="btn btn-primary" onClick={() => handleDelete(currentTemplate)}>Delete</button>
+                    <button id="closeModal" className="btn btn-link" onClick={() => handleClose()}>Close</button>
                 </div>
             </div>
         </Modal >
